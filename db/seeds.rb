ec73e80c927b3b9c8295ba2c9a7ce95e27fca246
George Gowers <georgegowers@gmail.com>
2022-05-19 22:15:25 +0200

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def ingredients_array(cocktail)
+
+  ingredients = ""
+  ingredient = cocktail["strIngredient1"]
+  number = 1
+
+  while !ingredient.nil? && ingredient != ""
+    if ingredients == ""
+      ingredients += ingredient
+    else
+      ingredients += ", " + ingredient
+    end
+    number += 1
+    ingredient = cocktail["strIngredient#{number}"]
+  end
+
+  return ingredients
+
+end
+
+
+Cocktail.destroy_all
+
+puts "destroyed all cocktails"
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+
+alphabet.each do |letter|
+  url = "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?f=#{letter}"
+  url = RestClient.get(url)
+  @response = JSON.parse(url)
+  cocktail_array = @response["drinks"]
+  if cocktail_array != nil
+    cocktail_array.each do |cocktail|
+      name = cocktail["strDrink"]
+      preparation = cocktail["strInstructions"]
+      image = cocktail["strDrinkThumb"]
+      ingredients = ingredients_array(cocktail)
+      Cocktail.create(name: name, ingredients: ingredients, preparation: preparation, image: image)
+      puts "created cocktail"
+    end
+  end
+end
