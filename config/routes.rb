20d5e308422a60906b07ce735e1a4a73c20d9127
George Gowers <georgegowers@gmail.com>
2022-05-19 22:16:47 +0200

    added show create and search routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :cocktails, only: [ :show, :create ] do
+        collection do
+          get :search
+        end
+      end
+    end
+  end
+
 end
