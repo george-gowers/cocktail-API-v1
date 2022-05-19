a74b9dee8f507a97990998076a331af430bfda30
George Gowers <georgegowers@gmail.com>
2022-05-19 22:21:36 +0200

    added api/v1/cocktails_controller

---
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::CocktailsController < Api::V1::BaseController
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def search
+    @cocktails = Cocktail.search_by_name(params[:name])
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :ingredients, :preparation, :image)
+  end
+
+  def render_error
+    render json: { errors: @cocktail.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+end
